0020bedb07ec0eccc613d51706086678b4d77699
Jay McClow <jmcclow@mediatemple.net>
2012-11-29 15:47:30 -0800

    Making things work a little better with img compar

--- gallery.py
+++ gallery.py
@@ -2,7 +2,9 @@
 import ConfigParser
 import time
 import math, operator 
-from PIL import Image
+import Image
+import numpy
+import sys
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
@@ -46,6 +48,7 @@ if __name__ == '__main__':
     url_to_slurp = config.get('source', 'url')
     url_to_login = config.get('source', 'login')
     username = config.get('source', 'user')
+    photo_url = config.get('source', 'photo_url')
     password = getpass() 
     igid = raw_input("Please enter igid: ") 
     browser = webdriver.Firefox()
@@ -55,7 +58,6 @@ if __name__ == '__main__':
     passwd = browser.find_element_by_name("password")
     passwd.send_keys(password + Keys.RETURN)
     time.sleep(1)
-    print "Going to " + url_to_slurp + igid
     browser.get(url_to_slurp + igid)
     time.sleep(9)
     photo_id = 0 
@@ -64,19 +66,24 @@ if __name__ == '__main__':
         photo_id = int(element.get_attribute('id'))
     except:
         print "Oh oh that was bad..."
-    img_url = str("http://imgs.sthlmsfinest.com/imageGalleryImages/scaled/" + str(photo_id) + "_102_73_1.jpg") 
-    print "Getting " + img_url 
+    image_url = str(photo_url + str(photo_id) + "_102_73_1.jpg") 
+    print "Getting " + image_url 
     while ( 1 ):
-        urllib.urlretrieve(img_url, str(photo_id) + '.jpg')
+        urllib.urlretrieve(image_url, str(photo_id) + '.jpg')
         image1 = Image.open(str(photo_id) + '.jpg')
         images = find_images('thumbnails')
-        print images[0]
-        image2 = Image.open(str('thumbnails/' + images[1]))
         h1 = image1.histogram()
-        h2 = image1.histogram()
-        rms = math.sqrt(reduce(operator.add,
-            map(lambda a,b: (a-b)**2, h1, h2))/len(h1))
-        print rms
+        for i in images:
+            image2 = Image.open(str('thumbnails/' + i))
+            h2 = image1.histogram()
+            s = 0
+            if image1.size != image2.size or image1.getbands() != image2.getbands():
+                break 
+            for band_index, band in enumerate(image1.getbands()):
+                m1 = numpy.fft.fft2(numpy.array([p[band_index] for p in image1.getdata()]).reshape(*image1.size))
+                m2 = numpy.fft.fft2(numpy.array([p[band_index] for p in image2.getdata()]).reshape(*image2.size))
+                s += numpy.sum(numpy.abs(m1-m2))
+            print s
         break
 #        try: 
 #            e = browser.find_element_by_name("imageDescription[" + photoid + "]")
@@ -90,5 +97,5 @@ if __name__ == '__main__':
 #            print "Couldn't find submit button"
 #            # Do phash on this image to what we have
 #            photo_id += 1
-#            img_url = "http://imgs.sthlmsfinest.com/imageGalleryImages/scaled/" + str(photo_id) + "_102_73_1.jpg" 
+#            image_url = "http://images.sthlmsfinest.com/imageGalleryImages/scaled/" + str(photo_id) + "_102_73_1.jpg" 
     browser.close()
