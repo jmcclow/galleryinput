1baec3ad7f2b1f26b19b8764c65b9d100b7ebcba
Jay McClow <jmcclow@mediatemple.net>
2012-11-30 11:54:06 -0800

    Made some changes so it actually works

--- gallery.py
+++ gallery.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python
 import ConfigParser
 import time
-import math, operator 
-import Image
-import numpy
 import sys
+import Image
+import pHash
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.keys import Keys
@@ -44,12 +43,12 @@ if __name__ == '__main__':
     config = ConfigParser.RawConfigParser()
     config.read('finest.cfg')
     inf = 'names.txt'
+    photo_to_id = defaultdict(str)
     name_to_num = get_names(inf)
     url_to_slurp = config.get('source', 'url')
     url_to_login = config.get('source', 'login')
     username = config.get('source', 'user')
     photo_url = config.get('source', 'photo_url')
-    suffix = config.get('source', 'suffix')
     password = getpass() 
     igid = raw_input("Please enter igid: ") 
     browser = webdriver.Firefox()
@@ -58,39 +57,46 @@ if __name__ == '__main__':
     user.send_keys(username)
     passwd = browser.find_element_by_name("password")
     passwd.send_keys(password + Keys.RETURN)
-    time.sleep(1)
     browser.get(url_to_slurp + igid)
-    time.sleep(9)
     photo_id = 0 
+    end_id = 0
     try:
         element = browser.find_element_by_xpath("//html/body/div/div/form/div")
-        photo_id = int(element.get_attribute('id'))
+        orig_photo_id = int(element.get_attribute('id'))
     except:
         print "Oh oh that was bad..."
-    image_url = str(photo_url + str(photo_id) + suffix) 
-    print "Getting " + image_url 
+    photo_id = orig_photo_id
     while ( 1 ):
+        image_url = str(photo_url + str(photo_id) + "_102_73_1.jpg") 
+        try:
+            e = browser.find_element_by_name('imageDescription[' + str(photo_id) + ']')
+        except:
+            print "We've reached all the images we could find"
+            end_id = photo_id - 1
+            break
         urllib.urlretrieve(image_url, str(photo_id) + '.jpg')
         image1 = Image.open(str(photo_id) + '.jpg')
         images = find_images('thumbnails')
-        h1 = image1.histogram()
-        for i in images:
-            image2 = Image.open(str('thumbnails/' + i))
-            h2 = image1.histogram()
-            s = 0
-            if image1.size != image2.size or image1.getbands() != image2.getbands():
-                break 
-            for band_index, band in enumerate(image1.getbands()):
-                m1 = numpy.fft.fft2(numpy.array([p[band_index] for p in image1.getdata()]).reshape(*image1.size))
-                m2 = numpy.fft.fft2(numpy.array([p[band_index] for p in image2.getdata()]).reshape(*image2.size))
-                s += numpy.sum(numpy.abs(m1-m2))
-            print s
-        break
-#        try: 
-#            e = browser.find_element_by_name("imageDescription[" + photoid + "]")
-#            e.send_keys("Description")
-#        except:
-#            print "Sorry couldn't find that element " + str(photoid)
+        photo_id += 1
+    photo_id = orig_photo_id
+    for i in images:
+        digest1 = pHash.image_digest(str(photo_id) + '.jpg', 1.0, 1.0, 180)
+        digest2 = pHash.image_digest(str('thumbnails/' + i), 1.0, 1.0, 180)
+        if pHash.crosscorr(digest1, digest2) == 1:
+            if photo_id < end_id:
+                photo_to_id[photo_id] = i
+                photo_id += 1
+
+        for ident in photo_to_id:
+            print photo_to_id[ident]
+            repl = re.sub('IMG_', '', str(photo_to_id[ident]))
+            photo_num = re.sub('.jpg', '', repl) 
+            if name_to_num[photo_num]:
+                try: 
+                    e = browser.find_element_by_name("imageDescription[" + str(ident) + "]")
+                    e.send_keys(name_to_num[photo_num])
+                except:
+                    print "Sorry couldn't find that element " + str(ident)
 #        try:
 #            e = browser.find_element_by_value("Spara")
 #            e.submit()
